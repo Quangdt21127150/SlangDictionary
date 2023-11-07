e95843ce76b4d8c7c9ea09bfd52c3f41cfd30153
nhnquang21@clc.fitus.edu.vn <138848005+Quangdt21127150@users.noreply.github.com>
2023-11-07 23:48:05 +0700

    Chức năng đố vui, chương trình hiển thị 1 definition, với 4 slang words đáp án cho người dùng chọn

--- Slang_Dictionary.java
+++ Slang_Dictionary.java
@@ -294,6 +294,49 @@ public class Slang_Dictionary{
         }
     }
 
+    public static void definitionQuiz() throws IOException, InterruptedException{
+        TreeMap<String, Set<String>> quiz = new TreeMap<String, Set<String>>();
+        for(int i = 0; i < 4; i++){
+            String word = onThisDaySlang(true);
+            quiz.put(word, data.get(word));
+        }
+        int randomAnswers = new Random().nextInt(4);
+
+        List<String> keysArray = new ArrayList<String>(quiz.keySet());
+        String question = keysArray.get(randomAnswers);
+
+        System.out.println();
+        System.out.println("Welcome to slang word quiz follow definition");
+        System.out.println("Choose slang word has this definition: " + quiz.get(question).iterator().next());
+
+        int i = 0;
+        Set<Map.Entry<String, Set<String>>> q = quiz.entrySet();
+        for(Map.Entry<String,Set<String>> item: q){
+            if (i % 2 == 0){
+                System.out.print((char)(i + 65) + ". " + item.getKey() + "\t\t");
+            }
+            else {
+                System.out.println((char)(i + 65) + ". " + item.getKey());
+            }
+            ++i;
+        }
+
+        String str = "";
+        while (!str.matches("[ABCD]{1}")) {
+            System.out.print("Choose correct answer (A, B, C or D): ");
+            str = scan.nextLine().trim();
+        }
+        int choice = (int)str.toCharArray()[0] - 65;
+
+        if (choice == randomAnswers)
+            System.out.println("Correct Answer! Congratulation!!!");
+        else {
+            char answers = (char)(randomAnswers + 65);
+            System.out.println("Incorrect! Good luck for next time!");
+            System.out.println("Correct answer is: " + answers + ". " + question);
+        }
+    }
+
     public static void main(String [] args) throws IOException, InterruptedException{
         inputFile("MyDictionary.txt");
         inputHistory();
@@ -358,6 +401,11 @@ public class Slang_Dictionary{
             }
             else if (choice.equals("9")){
                 slangWordQuiz();
+                System.out.print("Choose any key to back to menu: ");
+                choice = scan.nextLine();
+            }
+            else if (choice.equals("10")){
+                definitionQuiz();
                 System.out.print("Choose any key to back to menu: ");
                 choice = scan.nextLine();
             }
