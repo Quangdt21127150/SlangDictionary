f371c0767b39df47762b1a1d08fdd225b8d290ea
nhnquang21@clc.fitus.edu.vn <138848005+Quangdt21127150@users.noreply.github.com>
2023-11-07 19:43:58 +0700

    Chức năng add 1 slang words mới

--- Slang_Dictionary.java
+++ Slang_Dictionary.java
@@ -46,7 +46,7 @@ public class Slang_Dictionary{
 
     public static void searchSlang() throws IOException, InterruptedException{
         process.inheritIO().start().waitFor();
-        System.out.print("Enter slang word that you want to search: ");
+        System.out.print("Enter a slang word that you want to search: ");
         String input = scan.nextLine().trim().toUpperCase();
         addHistory(input);
 
@@ -70,7 +70,7 @@ public class Slang_Dictionary{
 
     public static void searchDefinition() throws IOException, InterruptedException{
         process.inheritIO().start().waitFor();
-        System.out.print("Enter definition that you want to search: ");
+        System.out.print("Enter a definition that you want to search: ");
         String def = scan.nextLine().trim();
 
         Boolean flag = false;
@@ -87,7 +87,7 @@ public class Slang_Dictionary{
         }
 
         if(!flag){
-            System.out.println("Can not find Slang Word has this definition!");
+            System.out.println("Can not find slang word has this definition!");
         }
     }
 
@@ -116,19 +116,57 @@ public class Slang_Dictionary{
     }
 
     public static void printHistory(){
-        if (history.isEmpty()){
+        if (history.isEmpty())
             System.out.println("History is empty");
-        }
-        else {
-            for(String word : history){
+        else 
+            for(String word : history)
                 System.out.println("\t" + "+ " + word);
-            }
+    }
+
+    public static void addSlangWord() throws IOException, InterruptedException{
+        Set<String> definition = new HashSet<String>();
+        process.inheritIO().start().waitFor();
+        System.out.print("Enter a slang word that you want to add: ");
+        String word = scan.nextLine().trim().toUpperCase();
+
+        String choice = "0";
+        if (data.containsKey(word)){
+            System.out.println();
+            System.out.println("Word exist in dictionary!");
+            System.out.println("1. Overwrite slang word");
+            System.out.println("2. Duplicate to new slang word");
+            System.out.print("Enter your choice: ");
+            choice = scan.nextLine();
         }
+        
+        System.out.println();
+        String numOfDef = "";
+        while(!numOfDef.matches("^\\d+$")){
+            System.out.print("Enter number of definition of slang word you want to add (it must be an integer): ");
+            scan.nextLine();
+        }
+        int c = Integer.parseInt(numOfDef);
+
+        for (int i = 0; i < c; ++i){
+            int k = i + 1;
+            System.out.print("Enter " + k + "st definition: ");
+            definition.add(scan.nextLine());
+        }
+
+        if (choice.equals("0") || choice.equals("1"))
+            data.put(word, definition);
+        else if (choice.equals("2"))
+            data.put(word.toLowerCase(), definition);   
+        else
+            return;
+
+        System.out.println("Add new slang word success!");
+        saveDictionary();
     }
 
     public static void main(String [] args) throws IOException, InterruptedException{
         inputFile("MyDictionary.txt");
-
+        inputHistory();
         String choice;
 
         while(true){
@@ -142,8 +180,8 @@ public class Slang_Dictionary{
 			System.out.println("6. Delete slang word");
 			System.out.println("7. Reset slang word Dictionary");
             System.out.println("8. On this day slang word");
-            System.out.println("9. Slang word quiz");
-            System.out.println("10. Slang word quiz follow definition");
+            System.out.println("9. slang word quiz");
+            System.out.println("10. slang word quiz follow definition");
 			System.out.println("Choose difference key to exit");
 			System.out.print("Choose your action: ");
 			choice = scan.nextLine();
@@ -160,6 +198,11 @@ public class Slang_Dictionary{
             }
             else if (choice.equals("3")){
                 printHistory();
+                System.out.print("Choose any key to back to menu: ");
+                choice = scan.nextLine();
+            }
+            else if (choice.equals("4")){
+                addSlangWord();
                 System.out.print("Choose any key to back to menu: ");
                 choice = scan.nextLine();
             }
